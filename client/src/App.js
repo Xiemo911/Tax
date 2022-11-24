f7359318372e534d32757a4db3b7cc379e2a1b35
aa <benasbu2003@gmail.com>
2022-11-24 13:46:05 +0200

    update1

--- client/src/App.js
+++ client/src/App.js
@@ -1,31 +1,26 @@
 import React, { Component } from "react";
 import "./App.css";
-import Navbar from "./Components/Layout/Navbar";
+// import Navbar from "./Components/Layout/Navbar";
 import { BrowserRouter as Router, Route } from "react-router-dom";
-import Footer from "./Components/Layout/Footer";
-import Home from "./Components/Layout/Home";
-import Social from "./Components/social/Social";
+// import Footer from "./Components/Layout/Footer";
+// import Home from "./Components/Layout/Home";
+// import Social from "./Components/social/Social";
 
 class App extends Component {
   state = {
-    isOn: []
-  }
-  connecToServer =() => {
+    isOn: [],
+  };
+  connecToServer = () => {
     fetch("/");
-  }
+  };
   componentDidMount() {
     this.connecToServer();
   }
   render() {
     return (
-      <Router>
-        {" "}
-        <div className="container">
-          {" "}
-          <Navbar /> <Route exact path="/" component={Home} />{" "}
-          <Route exact path="/social" component={Social} /> <Footer />{" "}
-        </div>{" "}
-      </Router>
+    
+        <div>jhhhjjhhjhjhjj</div>
+     
     );
   }
 }
