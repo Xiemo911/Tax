3f6729b6208242e51e7038fb650e01c84058a71e
aa <benasbu2003@gmail.com>
2022-11-24 13:32:17 +0200

    vhkvkhkhvhk

---
+++ server.js
@@ -0,0 +1,17 @@
+import express from "express";
+import path from "path";
+const port = process.env.PORT || 5000;
+const app = express();
+
+// app.use(express.static(path.join(__dirname, 'client/build')));
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "client/build")));
+  app.get("*", (req, res) => {
+    res.sendfile(path.join((__dirname = "client/build/index.html")));
+  });
+}
+
+// app.get("/", (req, res) => {
+//   res.send("Successful response.");
+// });
+app.listen(port, () => console.log("Example app is listening on port 5000."));
